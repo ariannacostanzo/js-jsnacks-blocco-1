25a505b1eb6160d7707e9417edc3547786a8f9b2
ariannacostanzo <costanzo.arianna@outlook.it>
2023-12-22 14:17:13 +0100

    finished first snack

---
+++ array-lista/js/main.js
@@ -0,0 +1,20 @@
+const fruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola']
+
+fruits.push('pesca')
+
+let isFound = false;
+
+for (let i = 0; i < fruits.length && !isFound; i++) {
+    const currentFruit = fruits[i];
+    
+    let message = "Oh no, devo uscire a comprare il cocomero!"
+    
+    if (currentFruit === 'cocomero') {
+        message = "Trovato! Devo solo preparare il cocktail."
+        isFound = true; 
+    } 
+
+    console.log(message)
+
+   
+}
