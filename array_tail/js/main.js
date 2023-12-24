c9b98b81f557d1a2db57344c2b49978a8dc3fe8d
ariannacostanzo <costanzo.arianna@outlook.it>
2023-12-24 21:02:27 +0100

    implementing bonus

--- array_tail/js/main.js
+++ array_tail/js/main.js
@@ -1,41 +1,72 @@
 const btn = document.getElementById('btn');
-const questionElement = document.getElementById('question'); 
-const errorBox = document.getElementById('error-box');
+const questionTotalNumbersElement = document.getElementById('question'); 
+const questionlastNumbersElement = document.getElementById('question-numbers-to-show')
+const errorBoxTotalNumbers = document.getElementById('error-box');
+const errorBoxLastNumbers = document.getElementById('error-box-numbers')
+const result = document.getElementById('result');
 const numbers = []
 
 
 btn.addEventListener('click', function () {
-    const number = parseInt(questionElement.value);
-    const numberStamp = number - 4
-    // ! Validazione
+    const number = parseInt(questionTotalNumbersElement.value);
+    const numberToShow = parseInt(questionlastNumbersElement.value)
+    const numberStamp = number - numberToShow 
     let message = ''
 
+    // ! Validazione
+    let isValid = true; 
+    let isValidSecond = true;
     
     if (isNaN(number) || number <= 5) {
-        message = 'Inserisci un numero maggiore di 5';
-        
+        isValid = false;
+    }   else if (isNaN(numberToShow) || numberToShow < 0 || numberToShow > number) {
+        isValidSecond = false;
     }
+
+    if (!isValid) {
+        message = 'Inserisci un numero maggiore di 5';
+        errorBoxTotalNumbers.innerText = message;
+        questionTotalNumbersElement.classList.add('is-invalid');
+        return;
+    } 
+    if (!isValidSecond) {
+        message = 'Il valore deve essere maggiore di zero ma non superiore al massimo numero di elementi della tua lista';
+        errorBoxLastNumbers.innerText = message;
+        questionlastNumbersElement.classList.add('is-invalid');
+        return;
+    }
+
     
-    errorBox.innerText = message;
+    errorBoxTotalNumbers.innerText = '';
+    questionTotalNumbersElement.classList.remove('is-invalid');
+    questionTotalNumbersElement.classList.add('is-valid');
+    errorBoxLastNumbers.innerText = '';
+    questionlastNumbersElement.classList.remove('is-invalid');
+    questionlastNumbersElement.classList.add('is-valid');
+
+
     
 
     // * Riempimento dell'array
 
-    // numbers.length = number
-
-    for (let i = 0; i <= number; i++) {
-        const randomNumbers = Math.floor(Math.random() * 100) + 1;
-        
-        numbers.push(randomNumbers)
-    }
-
-    for (let i = numberStamp; i < numbers.length; i++) {
-        console.log(numbers[i])
-    }
     
-    // console.log(numbers[numbers.length - 5])
-    console.table(numbers)
+    //inserisco tanti numeri random nell'array quanto è il valore dell'input dell'utente
+    for (let i = 1; i <= number; i++) {
+        const randomNumbers = Math.floor(Math.random() * 100) + 1;
+        numbers.push(randomNumbers);
+    }
 
-    //il mio number è la lunghezza del mio array mentre gli elementi dentro devono essere
-    //numeri random
+    message = `Gli ultimi ${numberToShow} numeri della tua lista sono: <ul>`
+
+    //stampo i numeri finchè ho elementi ma partendo dal numero che mi è stato dato - 4, stampando così gli ultimi 5
+    for (let i = numberStamp; i < numbers.length; i++) {
+        message += `<li>${numbers[i]}</li>`;
+        console.log(i);
+    };
+
+    message += '</ul>';
+    result.innerHTML = message;
+    console.table(numbers);
+
+    
 });
